99f8a96ea8e1d8b3877e75330e48a3b790142b75
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2020-02-18 13:36:04 -0600

    Clase fecha y probarFecha

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import Tiempo from './modulos/Tiempo.js'
 import Precio from './modulos/Precio.js';
+import Fecha from './modulos/Fecha.js';
+
 const tiempo = new Tiempo(9, 35, 'pm');
 const precio = new Precio(62562543.016);
+const ulisesFechaNacimiento = new Fecha(24, 9, 2000);
+
 const probarTiempo = _ => {
     console.log(
         tiempo.getFormato12(),
@@ -13,3 +17,14 @@ const probarPrecio = _ => {
 }
 probarPrecio();
 
+const probarFecha = _ => {
+    console.log(
+        ulisesFechaNacimiento.getAnnios(),
+        ulisesFechaNacimiento.getDiaFecha(),
+        ulisesFechaNacimiento.getDias(),
+        ulisesFechaNacimiento.getFecha(),
+        ulisesFechaNacimiento.getMeses(),
+        ulisesFechaNacimiento.getSemanas()
+    );
+}
+probarFecha();
