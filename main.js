8ed39401e18d201c06408ce52ac8c3529b5a64b4
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2020-02-18 19:07:35 -0600

    Clase Cliente y probarCliente

--- main.js
+++ main.js
@@ -2,24 +2,26 @@ import Tiempo from './modulos/Tiempo.js'
 import Precio from './modulos/Precio.js';
 import Fecha from './modulos/Fecha.js';
 import Direccion from './modulos/Direccion.js';
+import Cliente from './modulos/Cliente.js';
 
 const tiempo = new Tiempo(9, 35, 'pm');
 const precio = new Precio(62562543.016);
 const ulisesFechaNacimiento = new Fecha(24, 9, 2000);
 const ulisesDireccion = new Direccion('Belisario Dominguez', '39A', undefined, 'Obrera', 28510, 'Queseria', 'Cuahutemoc');
+const ulisesCliente = new Cliente('Ulises Ramirez', ulisesDireccion, 3123398831);
 
-const probarTiempo = _ => {
+const probarTiempo = _ =>
     console.log(
         tiempo.getFormato12(),
         tiempo.getFormato24());
-}
+
 probarTiempo();
-const probarPrecio = _ => {
+const probarPrecio = _ =>
     console.log(precio.getPrecio());
-}
+
 probarPrecio();
 
-const probarFecha = _ => {
+const probarFecha = _ =>
     console.log(
         ulisesFechaNacimiento.getAnnios(),
         ulisesFechaNacimiento.getDiaFecha(),
@@ -28,10 +30,11 @@ const probarFecha = _ => {
         ulisesFechaNacimiento.getMeses(),
         ulisesFechaNacimiento.getSemanas()
     );
-}
+
 probarFecha();
 
-const probarDireccion = _ => {
-    console.log(ulisesDireccion.getFormatoCorto(), '\n',ulisesDireccion.getFormatoLargo());
-}
+const probarDireccion = _ => console.log(ulisesDireccion.getFormatoCorto(), '\n', ulisesDireccion.getFormatoLargo());
 probarDireccion();
+
+const probarCliente = _ => console.log(ulisesCliente.getPerfil());
+probarCliente();
