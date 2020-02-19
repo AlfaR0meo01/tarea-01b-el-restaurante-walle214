af956e6131320317700b76183e85feeb72be812b
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2020-02-19 17:33:14 -0600

    getter del valor numerico

--- modulos/Precio.js
+++ modulos/Precio.js
@@ -6,6 +6,7 @@ export default class Precio {
     constructor(valor) {
         this.valor = valor;
     }
+    get precio() { return this.valor };
     getPrecio = _ => {
         const valorString = this.valor.toString();
         const enteros = valorString.split('.')[0].split('');
