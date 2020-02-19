f01bdda21a936eb95c874c1d37ab5341497166c0
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2020-02-18 19:00:12 -0600

    Clase Direccion y probarDireccion

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import Tiempo from './modulos/Tiempo.js'
 import Precio from './modulos/Precio.js';
 import Fecha from './modulos/Fecha.js';
+import Direccion from './modulos/Direccion.js';
 
 const tiempo = new Tiempo(9, 35, 'pm');
 const precio = new Precio(62562543.016);
 const ulisesFechaNacimiento = new Fecha(24, 9, 2000);
+const ulisesDireccion = new Direccion('Belisario Dominguez', '39A', undefined, 'Obrera', 28510, 'Queseria', 'Cuahutemoc');
 
 const probarTiempo = _ => {
     console.log(
@@ -28,3 +30,8 @@ const probarFecha = _ => {
     );
 }
 probarFecha();
+
+const probarDireccion = _ => {
+    console.log(ulisesDireccion.getFormatoCorto(), '\n',ulisesDireccion.getFormatoLargo());
+}
+probarDireccion();
